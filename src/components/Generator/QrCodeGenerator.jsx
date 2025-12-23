3c9512dd1fc3e57ada0154378497f0a1e60c6946
Andrew Abramov <andrabra@yandex.ru>
2025-12-24 02:16:35 +0500

    Подправил форму

--- src/components/Generator/QrCodeGenerator.jsx
+++ src/components/Generator/QrCodeGenerator.jsx
@@ -1,44 +1,43 @@
-import { QRCodeSVG } from "qrcode.react";
-import { useState } from "react";
+import { QRCodeSVG } from 'qrcode.react';
+import { useState } from 'react';
 import Button from '../common/button';
 
 const QrCodeGenerator = () => {
-  const [inputChar, setInputChar] = useState("");
-  const [resultShowQrCode, setResultShowQrCode] = useState("");
+  const [inputChar, setInputChar] = useState('');
+  const [resultShowQrCode, setResultShowQrCode] = useState('');
 
   const handleClick = (e) => {
     e.preventDefault();
 
     setResultShowQrCode(inputChar);
-    setInputChar("");
+    setInputChar('');
   };
 
   const handleChange = (e) => {
     setInputChar(e.target.value);
-    setResultShowQrCode("");
+    setResultShowQrCode('');
   };
 
   return (
-    <div className="flex flex-col justify-center items-center gap-5">
+    <div className='flex flex-col justify-center items-center gap-5'>
       <form
-        action=""
-        className="flex flex-col justify-center items-center gap-5"
+        action=''
+        className='flex flex-col justify-center items-center gap-5'
       >
         <input
-          placeholder="Type text..."
+          placeholder='Type text...'
           value={inputChar}
           onChange={handleChange}
-          className="border rounded-sm p-2"
-          type="text"
+          className='border rounded-sm p-2'
+          type='text'
         />
-        <Button
-          onClick={handleClick}>
+        <Button type='submit' onClick={handleClick}>
           Generate!
         </Button>
 
-        <div className="min-h-70">
-          {resultShowQrCode !== "" && (
-            <QRCodeSVG size="256" value={resultShowQrCode} />
+        <div className='min-h-70'>
+          {resultShowQrCode !== '' && (
+            <QRCodeSVG size='256' value={resultShowQrCode} />
           )}
         </div>
       </form>
--- src/components/common/button.jsx
+++ src/components/common/button.jsx
@@ -1,7 +1,7 @@
-const Button = ({children, onClick}) => {
+const Button = ({children, onClick, type = 'button'}) => {
   return (
     <button
-      type='button'
+      type={type}
       onClick={onClick}
       className='border rounded-sm p-2 cursor-pointer hover:gray-600 focus:ring-1 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none'
     >
