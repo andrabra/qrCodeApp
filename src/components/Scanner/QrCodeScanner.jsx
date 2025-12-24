abd164f04fdae0e82d182297c782631dde0da248
Andrew Abramov <andrabra@yandex.ru>
2025-12-24 20:54:02 +0500

    Сделал запись отсканированных кодов в localStorage

--- src/components/Scanner/QrCodeScanner.jsx
+++ src/components/Scanner/QrCodeScanner.jsx
@@ -10,21 +10,32 @@ const QrCodeScanner = () => {
     finder: false,
   };
 
+  const scannerStyles = {
+    container: {
+      width: '400px',
+      height: '400px',
+    },
+  };
+
   const scanHandler = (result) => {
     const resultValue = result[0].rawValue;
+    const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]');
 
     setScannedQrCode(resultValue);
 
-    const prevData = JSON.parse(localStorage.getItem(SCAN_DATA)) || [];
-    localStorage.setItem(SCAN_DATA, [...prevData, resultValue]);
+    if (prevData.includes(resultValue)) {
+      return;
+    }
+
+    localStorage.setItem(SCAN_DATA, JSON.stringify([...prevData, resultValue]));
   };
 
   return (
     <div className='flex flex-col justify-center items-center gap-5'>
       <Scanner
         onScan={scanHandler}
-        allowMultiple
         components={scannerSettings}
+        styles={scannerStyles}
         onError={(error) => console.log(error?.message)}
       />
       {scannedQrCode && <p>{scannedQrCode}</p>}
